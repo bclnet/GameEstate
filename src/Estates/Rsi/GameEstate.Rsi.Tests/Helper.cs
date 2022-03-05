6cb0845f0c946cd1da60f16ce828f11981ff6f2a
Sky Morey <smorey@degdigital.com>
2022-03-04 19:09:44 -0600

    updates

---
+++ src/Estates/Rsi/GameEstate.Rsi.Tests/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameEstate.Rsi
+{
+    public static class Helper
+    {
+        static readonly Estate estateRsi = EstateManager.GetEstate("Rsi");
+
+        public static readonly Dictionary<string, Lazy<EstatePakFile>> Paks = new()
+        {
+            { "Rsi:StarCitizen", new Lazy<EstatePakFile>(() => estateRsi.OpenPakFile(new Uri("game:/Data.p4k#StarCitizen"))) },
+        };
+    }
+}
